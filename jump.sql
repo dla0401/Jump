b25c29a4778d37b7d1e47461cdd850563c10f989
ChelHyun <ChelHyun@LimChelHyun>
2014-10-16 12:01:50 +0900

    sql 완료(2014-10-16 오전 12:01)

--- jump.sql
+++ jump.sql
@@ -6,16 +6,25 @@ create table jump(
 )
 
 create sequence board_num;
-create table jump_board(
+
+create table jumpBoard(
 	jump_id varchar2(1000) not null,
-	board_num number primary key,
+	board_num number primary key, /*create sequence board_num*/
 	board_title varchar2(250) not null,
 	board_content varchar2(4000),
 	foreign key(jump_id) references jump(jump_id)
 )
-create table jump_check(
+
+create table jumpImage(
+	board_num number not null,
+	image_date date default sysdate,
+	image varchar2(100) not null,
+	foreign key(board_num) references jumpBoard(board_num)
+)
+
+create table jumpCheck(
 	board_num number not null,
 	check_best number default 0,
 	check_worst number default 0,
-	foreign key(board_num) references jump_board(board_num)
+	foreign key(board_num) references jumpBoard(board_num)
 )
